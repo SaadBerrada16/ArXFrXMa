80a2b15443e951b8905974b80088fc5162937a5c
gpadula <gianni.padula18@gmail.com>
2022-11-23 22:07:29 +0100

    primer implementacion nodo

--- nodo.hpp
+++ nodo.hpp
@@ -2,31 +2,83 @@
 #define NODO_H_INCLUDED
 
 #include "Animal.hpp"
+using namespace std;
 
-typedef Animal* Dato;
+#define M 3
+#define medio (M-1)/2
 
+template <typename Tipo>
 class Nodo {
-    // Atributos
-private:
-    Dato dato;
-    Nodo* siguiente;
+    private:
+        bool eliminado[M];
+        string claves[M];
+        Tipo datos[M];
+        bool esHoja;
+        Nodo* antecesor;
+        Nodo* vias[M+1];
+        void dividirNodo();
+    public:
+        void insertar(string clave, Tipo dato);
 
-public:
-    // Constructor
-    // crea un nodo con dato = d y siguiente = 0
-    Nodo(Dato d);
+        bool contieneClave(string clave);
 
-    // pre: que existe un nodo pn
-    // pos: cambia el nodo a cual apunta
-    void cambiar_siguiente(Nodo* pn);
+        Tipo obtenerDato(string clave);
 
-    // pre: que existe un nodo
-    // pos: devuelve el dato del nodo
-    Dato obtener_dato();
-
-    // pre: que existe el siguiente
-    // pos: devuelve el nodo siguiente
-    Nodo* obtener_siguiente();
+        Tipo eliminar(string clave);     
 };
+
+template <typename Tipo>
+void Nodo<Tipo>::dividirNodo() {
+    clave = claves[medio];
+    dato = datos[medio];
+    
+}
+
+template <typename Tipo>
+void Nodo<Tipo>::insertar(string clave, Tipo dato) {
+    int  i = 0;
+    while (claves[i] != NULL && clave > claves[i] && i < M)
+       i++;
+    if (esHoja) {
+        for (int j = M-1; j > i; j--) {
+            claves[j] = claves[j-1];
+            datos[j] = datos[j-1];
+        }
+        claves[i] = clave;
+        datos[i] = dato;
+        if (claves[M-1] != NULL) 
+            dividirNodo();
+    }
+    else {
+        vias[i]->insertar(clave, dato);
+    }
+}
+
+template <typename Tipo>
+bool Nodo<Tipo>::contieneClave(string clave) {
+    int  i = 0;
+    while (clave >= claves[i] && i < M) {
+        if (clave == claves[i])
+            return true;
+        i++;
+    }
+    vias[i]->contieneClave(clave);
+}
+
+template <typename Tipo>
+Tipo Nodo<Tipo>::obtenerDato(string clave) {
+    int  i = 0;
+    while (clave >= claves[i] && i < M) {
+        if (clave == claves[i])
+            return datos[i];
+        i++;
+    }
+    vias[i]->obtenerDato(clave);
+}
+
+template <typename Tipo>
+Tipo Nodo<Tipo>::eliminar(string clave) {
+
+}
 
 #endif // NODO_H_INCLUDED
