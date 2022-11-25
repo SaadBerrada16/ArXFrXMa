b3f190091fc17e2870c07c1fdc9e804cecfcf821
root <root@DESKTOP-HKN66HV.localdomain>
2022-11-25 15:56:28 +0100

    Auto modif

--- Auto.hpp
+++ Auto.hpp
@@ -20,12 +20,16 @@ class Auto {
         void bajar_combustible(int litros);
 
         // pre: que existe el auto y que se pide al usuario cuanto quiere cargar
-        // pos: baja el combustible de 5 litros
-        void bajar_combustible();
+        // pos: aumenta el combustible de 5 litros
+        void cargar_combustible();
 
         // pre: que existe el auto y que se pide al usuario cuanto quiere cargar
         // pos: aumenta el combustible de x litros
         void cargar_combustible(int litros);
+
+        // pre: que existe el auto y que indique un costo por un destino
+        // pos: devuelve si si o no hay suficiente combustible
+        bool combustible_suficiente(int costo);
 };
 
 #endif
