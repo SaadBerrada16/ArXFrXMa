20286c1b2a50f734deded3be0ebbcf451ec65c52
root <root@LAPTOP-9H8VQ8KQ.localdomain>
2022-12-01 22:21:59 -0300

    comentario

--- BST.hpp
+++ BST.hpp
@@ -13,9 +13,21 @@ private:
     BSTNode* root;
 
     //métodos
+
+    // pre: -
+    // pos: Inserta en el nodo los datos del animal.
     BSTNode* insert(BSTNode* node, Animal* data);
+
+    // pre: -
+    // pos: Imprime los datos del BST de manera recursiva desde el nodo, ordenados del menor al mayor valor.
     void print_in_order(BSTNode * node);
+
+    // pre: -
+    // pos: Guarda en el vector los datos almacenados a la posicion i del BST, ordenados del menor al mayor valor.
     void save_in_order(BSTNode * node, Animal** vector, int& i);
+
+    // pre: -
+    // pos: Elimina el nodo y sus atributos. 
     void delete_all(BSTNode* node);
 
 public:
